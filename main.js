f17a76897f2d438e2ffa38f73877bd8a866d0d37
maggie_e_matthias <magdolinemad2007@gmail.com>
2026-09-14 22:25:52 +0300

    added function for reading music metadata from users device

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// App window creation
+
 require('electron-reload')(__dirname)
 const {app, BrowserWindow, ipcMain} = require('electron');
 
@@ -24,6 +26,51 @@ function createWindow(){
     ipcMain.on('window:minimize', () => win.minimize());
     ipcMain.on('window:close', () => win.close());
     
+
+    // Do the scanning of data
+
+    scanMusicFolder().then(tracks =>{
+        console.log(tracks);
+    });
 }
 
 app.whenReady().then(createWindow);
+
+
+// Music Meta Data Reading
+
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+const mm = require('music-metadata');
+
+async function scanMusicFolder(){
+    // 1. Find user's home directory and the Music subfolder path
+    const musicFolderPath = path.join(os.homedir(), 'Music');
+
+    // 2. Get the files in that folder
+    const files = fs.readdirSync(musicFolderPath);
+
+    // 3. Keep only audio files (using a filter with extension)
+    const audioFiles = files.filter(file => file.endsWith('.mp3'));
+
+    //  4. Extract metadata of each audio file
+    const tracks = [];
+
+    for (const fileName of audioFiles){
+        const fullPath = path.join(musicFolderPath, fileName);
+        const metadata = await mm.parseFile(fullPath);
+
+        tracks.push({
+            title: metadata.common.title || fileName,
+            artist: metadata.common.artist || "Unknown Artist",
+            album: metadata.common.album || "Unknown Album", 
+            filePath: fullPath
+        });
+
+    }
+
+    return tracks;
+}
+
+module.exports = {scanMusicFolder};
